9dd130a70da4d5fbff9482cc3b6a8fff523490eb
George8918 <GZheng8918@gmail.com>
2023-07-16 14:17:23 -0700

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,24 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    fib_sequence = [0, 1]  # Starting with the initial Fibonacci numbers
+
+    if length < 0:
+        print("Invalid length. Please provide a non-negative integer.")
+        return
+
+    if length == 0:
+        print([])  # Print an empty list
+        return
+    
+    if length == 1:
+        print([fib_sequence[0]])  # Print a single element list
+        return
+
+    if length == 2:
+        print(fib_sequence)  # Print the initial Fibonacci numbers as a list
+        return
+
+    while len(fib_sequence) < length:
+        next_num = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_num)
+
+    print(fib_sequence) 
